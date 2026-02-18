2f06b60b6807073a1b0c984927d8a4c3fc2c29cf
T.B <153681972+typingthomas@users.noreply.github.com>
2026-02-18 14:46:43 -0700

    Fix indentation in get_local_ip function

--- facelog/webserver.py
+++ facelog/webserver.py
@@ -32,7 +32,7 @@ HTML_TEMPLATE = """
 def get_local_ip():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))  # Doesn't actually send data
+        s.connect(("8.8.8.8", 80))  
         ip = s.getsockname()[0]
         s.close()
         return ip
